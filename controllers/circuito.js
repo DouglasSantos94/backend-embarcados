e9bdaa654feb21754aabbab329d2a5104e1f7e88
Douglas Santos <dgstos94@gmail.com>
2024-11-12 21:20:35 -0300

    create controllers

--- controllers/circuito.js
+++ controllers/circuito.js
@@ -1,5 +1,32 @@
 module.exports = {
   getStatusLampada: async (req, res) => {
-    
+    const statusLampada = true;
+
+    res.status(200).send(statusLampada);
+  },
+  getModoLampada: async (req, res) => {
+    const modoLampada = true;
+
+    res.status(200).send(modoLampada);
+  },
+  getStatusPersiana: async (req, res) => {
+    const statusPersiana = true;
+
+    res.status(200).send(statusPersiana);
+  },
+  alterarStatusLampada: async (req, res) => {
+    const statusLampada = true;
+
+    res.status(200).send(statusLampada);
+  },
+  alterarModoLampada: async (req, res) => {
+    const modoLampada = true;
+
+    res.status(200).send(modoLampada);
+  },
+  alterarStatusPersiana: async (req, res) => {
+    const statusPersiana = true;
+
+    res.status(200).send(statusPersiana);
   }
 }

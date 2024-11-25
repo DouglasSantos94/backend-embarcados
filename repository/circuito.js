56589ced0b917288c98b43837123daf9964b2141
Douglas Santos <dgstos94@gmail.com>
2024-11-25 17:49:09 -0300

    update put repositories

--- repository/circuito.js
+++ repository/circuito.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
 const uri = 'mongodb+srv://embarcados:emb160@cluster0.7qtvn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const client = new MongoClient(uri);
@@ -13,22 +13,22 @@ module.exports = {
   statusLampada: () => collection.findOne(),
   modoLampada: () => collection.findOne(),
   statusPersiana: () => collection.findOne(),
-  // abrirPersiana: () => {
-  //   status.persianaAberta = true;
-  // },
-  // fecharPersiana: () => {
-  //   status.persianaAberta = false;
-  // },
-  // acenderLampada: () => {
-  //   status.lampadaLigada = true;
-  // },
-  // apagarLampada: () => {
-  //   status.lampadaLigada = false;
-  // },
-  // setModoLampadaManual: () => {
-  //   status.lampadaManual = true;
-  // },
-  // setModoLampadaAutomatico: () => {
-  //   status.lampadaManual = false;
-  // },
+  abrirPersiana: () => {
+    collection.updateOne({_id: new ObjectId('6744d708089802821641aed0')}, {$set: {persianaAberta: true}});
+  },
+  fecharPersiana: () => {
+    collection.updateOne({_id: new ObjectId('6744d708089802821641aed0')}, {$set: {persianaAberta: false}});  
+  },
+  acenderLampada: () => {
+    collection.updateOne({_id: new ObjectId('6744d708089802821641aed0')}, {$set: {lampadaLigada: true}});  
+  },
+  apagarLampada: () => {
+    collection.updateOne({_id: new ObjectId('6744d708089802821641aed0')}, {$set: {lampadaLigada: false}});  
+  },
+  setModoLampadaManual: () => {
+    collection.updateOne({_id: new ObjectId('6744d708089802821641aed0')}, {$set: {lampadaManual: true}});  
+  },
+  setModoLampadaAutomatico: () => {
+    collection.updateOne({_id: new ObjectId('6744d708089802821641aed0')}, {$set: {lampadaManual: false}});  
+  }
 }
